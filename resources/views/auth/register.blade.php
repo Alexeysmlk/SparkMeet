d7f70c0c255c22eee0404278d7832c6e62616aaf
Alexey Samoylik <samoilikaleksey@yandex.ru>
2023-06-17 21:13:07 +0300

    Add auth template and setting authorization

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,61 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
+    <div class="login-register-bg">
+        <div class="row no-gutters">
+            <div class="col-lg-6">
+                <div class="lg-left">
+                    <div class="lg-logo">
+                        <a href="index.html"><img src="{{asset('images/login-register/logo.svg')}}" alt=""></a>
+                    </div>
+                    <div class="lr-text">
+                        <h2>Register Now</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum blandit felis a
+                            hendrerit.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="lr-right">
+                    <h4>Sign Up to Goeveni</h4>
+                    <div class="login-register-form">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="form-group">
+                                <input class="form-input h_50 @error('email') is-invalid @enderror" type="email"
+                                       placeholder="Email Address" name="email" value="{{ old('email') }}">
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                            <div class="form-group">
+                                <input class="form-input h_50 @error('password') is-invalid @enderror" type="password"
+                                       placeholder="Password" name="password">
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            <div class="form-group">
+                                <input class="form-input h_50" type="password"
+                                       placeholder="Confirm password" name="password_confirmation">
                             </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
+                            <div class="rgstr-dt-txt">
+                                By clicking Sign Up, you agree to our <a href="#">Terms</a>, <a href="#">Data Policy</a>
+                                and <a href="#">Cookie Policy</a>. You may receive Email notifications from us and can
+                                opt out at any time.
                             </div>
-                        </div>
-                    </form>
+                            <button class="login-btn" type="submit">Register Now</button>
+                            <div class="login-link">If you have an account? <a href="{{route('login')}}">Login Now</a>
+                            </div>
+                        </form>
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
