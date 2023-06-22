fdebc6c105f3cb912ce9aa5a150272a279efed8d
Alexey Samoilik <samoilikaleksey@yandex.ru>
2023-06-22 22:56:50 +0000

    Adding a library with roles

---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $adminRole = Role::create(['name' => 'Admin']);
+        $userRole = Role::create(['name' => 'User']);
+    }
+}
