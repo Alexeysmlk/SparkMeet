8293e05ce59ac3f5d019a9d5802b42c64a00a0cf
Alexey Samoylik <samoilikaleksey@yandex.ru>
2023-06-26 18:10:00 +0300

    Fix problems with views

--- resources/views/client/profile/show.blade.php
+++ resources/views/client/profile/show.blade.php
@@ -61,7 +61,7 @@
                                                 <span
                                                     class="event-type">{{__($event->city->name)}}</span>
                                                 <p class="ingo-counter">
-                                                    <span>25 Interested</span>
+                                                    <span>{{$event->likedByUsers->count()}} Interested</span>
                                                 </p>
                                                 <div class="group-btns">
                                                     <a class="interest-btn btn-hover d-flex justify-content-center align-items-center" href="{{route('user.events.show', ['event' =>$event])}}"><i class="feather-eye me-2"></i>View</a>
