ac4f7063c56546c19f24876875142334a4f0bf27
Alexey Samoylik <samoilikaleksey@yandex.ru>
2023-06-26 16:10:04 +0300

    Add middleware for admin panel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'check.profile' => \App\Http\Middleware\CheckProfileExistence::class,
+        'role' => \App\Http\Middleware\CheckRole::class,
     ];
 }
